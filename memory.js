745eca08d451d184c438c3265429619526cf7b1f
Ismail Jaafari <iijaafari@gmail.com>
2015-12-18 10:21:40 -0500

    3rd day of working project

--- memory.js
+++ memory.js
@@ -6,30 +6,37 @@ $(document).ready(function() {
    var numMatches = 0;
    var flipped = [];
    var numArray = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10,11,11,12,12,13,13,14,14,15,15];
+
+// First methode to shuffle a set of HTML divs   
+$(function () {
+    var parent = $("#board");
+    var divs = parent.children();
+    while (divs.length) {
+        parent.append(divs.splice(Math.floor(Math.random() * divs.length), 1)[0]);
+    }
+});
+
+// Second Method
+   // function shuffle(array) {
+   //  var currentIndex = array.length, 
+   //                     temporaryValue,
+   //                     randomIndex;
+
+   //  while (0 !== currentIndex) {
+    
+   //      randomIndex = Math.floor(Math.random() * currentIndex);
+   //      currentIndex = currentIndex - 1;
+
+   //      temporaryValue = array[currentIndex];
+   //      array[currentIndex] = array[randomIndex];
+   // //      array[randomIndex] = temporaryValue;
+   //  } 
+
+   //  return array;
+   // }
+
    
-   function shuffle(array) {
-    var currentIndex = array.length, 
-                       temporaryValue,
-                       randomIndex;
-
-    // While there remain elements to shuffle...
-    while (0 !== currentIndex) {
-
-        // Pick a remaining element...
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex = currentIndex - 1;
-
-        // And swap it with the current element.
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    } 
-
-    return array;
-   }
-
-   
-   shuffle(numArray);
+   // shuffle(numArray);
    
    
    for (var i = 0; i < numArray.length; i++) {
@@ -38,8 +45,8 @@ $(document).ready(function() {
    };
    
    function matchCards(obj) {
-    // while(arr[randomIndex])
-      if (CardOne === CardTwo && flipped[0].attr('id')!==flipped[1].attr('id')) {
+    
+      if (CardOne === CardTwo ) {
           
           flipped[0].css("background-color", 'green');
           flipped[1].css("background-color", 'green');
@@ -54,7 +61,7 @@ $(document).ready(function() {
       } else {
           flipped[0].css("background-color", 'red');
           flipped[1].css("background-color", 'red');
-          $("#messages").append("No match. Try again :( ");
+          $("#messages").append("Sorry, try again :( ");
           
     setTimeout(function() {
           flipped[0].css("background-color", 'black');
@@ -68,16 +75,20 @@ $(document).ready(function() {
     // console.log(e.target.id);
     $("#messages").empty();
     $(this).css("background-color", 'black');
+    $('.card').rotate(180);
+
 
     if (cardsFlipped === 0) {
         CardOne = $(this).text();
         flipped[0] = $(this);
         cardsFlipped = 1;
+        $('.card').rotate(180);
+       
 
         
     } else if(e.target.id != flipped[0].id){
-        console.log(e.target.id);
-        console.log(flipped);
+        // console.log(e.target.id);
+        // console.log(flipped);
         // console.log(e.target.id === flipped[0].attr('id'));
         CardTwo = $(this).text();
         flipped[1] = $(this);
